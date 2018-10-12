102acbb7013482b424020ccbabd1900371f0605f
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2018-10-11 21:52:25 -0500

    Updates to clase 02 use of picker

--- Picker array/Picker array/ViewController.swift
+++ Picker array/Picker array/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var countries = [String]()
+    var opt1:Array<String>?
+    var opt2:[String]?
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var myPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countries = ["Perú", "Chile", "Ecuador", "Colombia", "Bolivia"];
+        countries.append("Uruguay");
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
+extension ViewController {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "Selecciona: \(countries[row])"
+    }
+    
+}
